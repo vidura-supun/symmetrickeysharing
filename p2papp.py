a7414aca0991649321f7c73bb5c80d0d5bd98c0b
vidura-supun <vidurasupun@gmail.com>
2019-02-08 22:57:13 +0530

    message verification using sha256 added tested

--- p2papp.py
+++ p2papp.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import hashlib
 import socket
 import threading
 from cryptography.fernet import Fernet
@@ -9,7 +10,7 @@ print("\nEnter 'exit' to quit\n")
 
 if(answer == 'server'):
     HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-    PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+    PORT = 65433        # Port to listen on (non-privileged ports are > 1023)
     print('Listening. . .')
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
@@ -20,8 +21,20 @@ if(answer == 'server'):
             while True:
                 # print out what the client sends
                 data = de_message.decrypt(client_socket.recv(1024))
+
+                #seperating message and the hash
+                mess = data.decode("utf-8")[0:len(data.decode("utf-8"))-64]
+                sha_hash = data.decode("utf-8")[len(data.decode("utf-8"))-64:]
                 
-                print("[*] Received: %s" % data.decode("utf-8"))
+                
+                print("[*] Received: %s" % mess)
+                #re hashing for comparison
+                sha_hash2 = hashlib.sha256(bytes(mess, 'utf8')).hexdigest()
+                print("\nIntegrity Verificaion....")
+                if(sha_hash==sha_hash2):
+                    print("~VERIFIED~")
+                else:
+                    print("Verification Failed")
                 #send back a packet
                 client_socket.send("ACK!".encode())
        
@@ -40,13 +53,19 @@ if(answer == 'client'):
     #plain_text = cipher_suite.decrypt(b"gAAAAABcW-t3SP5NWJpJCMqHR1fkURoffr_6HAvqV_PAmag09Drtj9syhwuWvxiMOiUlEBfbWlUf1Zn1dp3FzUf3tn6E2vazrTvUXfUD78u_JqUFJ-RWysvk9Zv8mKHjLf-GGVC-w1cQ").decode("utf-8")
 
     HOST = '127.0.0.1'  # The server's hostname or IP address
-    PORT = 65432        # The port used by the server
+    PORT = 65433        # The port used by the server
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         while(True):
-            message = input("\nEnter message: ")
-            cli_rep = cipher_suite.encrypt(bytes(message, 'utf8'))
+            message = input("\nEnter message: ") 
+            #hashing of the message and adding it to the message
+            n_message = message + hashlib.sha256(bytes(message, 'utf8')).hexdigest()
+            cli_rep = cipher_suite.encrypt(bytes(n_message, 'utf8'))
+            
+            
+            
+            
             #print(cipher_suite.decrypt(cli_rep).decode("utf-8"))
             if(cipher_suite.decrypt(cli_rep) == b'exit'):
                 break
---
+++ sendmail.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import smtplib
+import getpass
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+ 
+fromaddr = input("enter your email address : ") 
+password = getpass.getpass("enter your password : ")
+toaddr = input("enter recievers email address : ")
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+
+sub = input("enter message subject : ")
+
+msg['Subject'] = sub
+ 
+body = input("enter the message body : ")
+msg.attach(MIMEText(body, 'plain'))
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr,password)
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
+print("email sent succesfully!") 
