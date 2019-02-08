ac42ca0a2e67e837043a8c84793d1960a1e62f71
vidura-supun <vidurasupun@gmail.com>
2019-02-08 20:46:16 +0530

    p2papp.py is now capable of encrypting and decrypting messages Key must be manually entered to the server

--- crypto.py
+++ crypto.py
@@ -8,4 +8,5 @@ cipher_suite = Fernet(key)
 #cipher_text = cipher_suite.encrypt(b"A really secret message. Not for prying eyes.")
 plain_text = cipher_suite.decrypt(b"gAAAAABcW-t3SP5NWJpJCMqHR1fkURoffr_6HAvqV_PAmag09Drtj9syhwuWvxiMOiUlEBfbWlUf1Zn1dp3FzUf3tn6E2vazrTvUXfUD78u_JqUFJ-RWysvk9Zv8mKHjLf-GGVC-w1cQ").decode("utf-8")
 
-print(plain_text)
+print("hey\n\n")
+print("hey")
--- p2papp.py
+++ p2papp.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 import socket
 import threading
+from cryptography.fernet import Fernet
+
 
 answer = input("Choose an option~\nClient or Server : ").lower()
+print("\nEnter 'exit' to quit\n")
 
 if(answer == 'server'):
     HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
@@ -13,10 +16,12 @@ if(answer == 'server'):
         s.listen(5)
         
         def handle_client(client_socket):
+            de_message = Fernet(bytes(input("Enter the secret key: "), 'utf8')) 
             while True:
                 # print out what the client sends
-                data = client_socket.recv(1024)
-                print("[*] Received: %s" % data)
+                data = de_message.decrypt(client_socket.recv(1024))
+                
+                print("[*] Received: %s" % data.decode("utf-8"))
                 #send back a packet
                 client_socket.send("ACK!".encode())
        
@@ -26,20 +31,30 @@ if(answer == 'server'):
             client_handler.start()
 
 if(answer == 'client'):
+
+    key = Fernet.generate_key() #generation of the key
+    print("\nkey generated. . .\n\n--Start of key--\n" + key.decode("utf-8") + "\n--End of key--")
+
+    cipher_suite = Fernet(key)
+
+    #plain_text = cipher_suite.decrypt(b"gAAAAABcW-t3SP5NWJpJCMqHR1fkURoffr_6HAvqV_PAmag09Drtj9syhwuWvxiMOiUlEBfbWlUf1Zn1dp3FzUf3tn6E2vazrTvUXfUD78u_JqUFJ-RWysvk9Zv8mKHjLf-GGVC-w1cQ").decode("utf-8")
+
     HOST = '127.0.0.1'  # The server's hostname or IP address
     PORT = 65432        # The port used by the server
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         while(True):
-            
-            cli_rep = input("Enter your message: ")
-            if(cli_rep == 'exit'):
+            message = input("\nEnter message: ")
+            cli_rep = cipher_suite.encrypt(bytes(message, 'utf8'))
+            #print(cipher_suite.decrypt(cli_rep).decode("utf-8"))
+            if(cipher_suite.decrypt(cli_rep) == b'exit'):
                 break
             else:
-                s.sendall(cli_rep.encode())
+                s.sendall(cli_rep)
+              
                 data = s.recv(1024)
-                print('Received', repr(data))
+                print('\nSuccess')
 
 
 
